733735956a02cb72ed356c56ab941c5891070fce
Elif Ozun <ozunelif0@gmail.com>
2024-02-05 13:51:14 +0300

    Business layer is extended

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Business;
+using Entities;
+using System;
+
+
+public class PersonManager
+{
+    //encapsulation
+    //Metod == Fonksiyon --> siklikla kullanacagimiz operasyonlar icin
+    public void ApplyForMask(Person person)
+    {
+
+    }
+}
