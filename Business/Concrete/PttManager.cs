9519fd7113dac0fac240977cce1c9de7919ebd91
Elif Ozun <ozunelif0@gmail.com>
2024-02-06 10:07:37 +0300

    GiveMask method is fixed

--- Business/Concrete/PttManager.cs
+++ Business/Concrete/PttManager.cs
@@ -1,10 +1,19 @@
 ﻿namespace Business;
+using Entities;
+using System;
 
+// bir sinif baska bir sinifi kullanırken (entity sinifinda gecerli degil) new'liyorsa ileride bu yazilimda degisiklik talebi geldiginde uygulama direnc gosterir.
 public class PttManager
 {
-    public void GiveMask()
+    public void GiveMask(Person person)
     {
-        
+        //iste burda bagimlilik olustu. 
+        PersonManager personManager = new PersonManager();
+
+        if(personManager.CheckPerson(person)) //personManager.Checkperson(person) == true
+        {
+            Console.WriteLine(person.FirstName + " icin maske verildi.");
+        } 
     }
 
 }
