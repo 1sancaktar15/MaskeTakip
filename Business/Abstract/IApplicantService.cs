5d9e6f27012957fb2a34f4e0779e65db7f5a8c0a
Elif Ozun <ozunelif0@gmail.com>
2024-02-06 09:56:47 +0300

    Interface added for applicants

---
+++ Business/Abstract/IApplicantService.cs
@@ -0,0 +1,18 @@
+﻿namespace Business;
+using Entities;
+
+//bir class'i olusturdugun zaman onun interface'ini olusturdugundan emin ol. 
+//aslinda once interface olusturma ile baslanir.
+public interface IApplicantService
+{
+    //burda sadece imzalar olusturulur.
+
+    //default olarak public'dir
+    void ApplyForMask(Person person);
+
+    List<Person> GetList();
+
+    //TC vatandasi mi kontrol edilir.
+    bool CheckPerson(Person person);
+
+}
