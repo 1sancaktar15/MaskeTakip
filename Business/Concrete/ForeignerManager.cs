97b2a9db78ef6a5f5a82ea545c22ae30a05f0a5f
Elif Ozun <ozunelif0@gmail.com>
2024-02-06 10:42:11 +0300

    ForeignManager created from class template

---
+++ Business/Concrete/ForeignerManager.cs
@@ -0,0 +1,21 @@
+﻿namespace Business;
+using Entities;
+
+public class ForeignerManager:IApplicantService
+{
+    public void ApplyForMask(Person person)
+    {
+
+    }
+
+    public List<Person> GetList()
+    {
+        return null;
+    }
+
+    //TC vatandasi mi kontrol edilir.
+    public bool CheckPerson(Person person)
+    {
+        return true;
+    }
+}
